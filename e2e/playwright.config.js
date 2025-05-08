1e2d7b053c50e0425e36830f38a25aa1cda49434
Ethan Dissmeyer <edissmeyer63342355@gmail.com>
2025-05-08 15:37:17 +1200

    Set up Playwright tests and config

---
+++ e2e/playwright.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+
+  use: {
+    trace: 'on-first-retry',
+    headless: false, // default, will still be overridden in project below
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        headless: false,         // explicitly run with visible browser
+      },
+    },
+  ],
+});
