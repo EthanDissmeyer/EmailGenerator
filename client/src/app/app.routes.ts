5b3df29ac4652f212f564ead9ece235861c82ff3
jude <jude@jjudell>
2025-05-11 21:39:41 +1200

    set up frontend

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EmailGeneratorComponent } from './pages/email-generator/email-generator.component';
+import { ClientSelectorComponent } from './components/client-selector/client-selector.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent, 
+        title: 'Home',
+    },
+    {
+        path: 'email-generator',
+        component: EmailGeneratorComponent,
+        title: 'Email Generator',
+    }
+
+];
---
+++ client/src/app/components/client-selector/client-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-selector.component.html',
+  styleUrl: './client-selector.component.css'
+})
+export class ClientSelectorComponent {
+
+}
