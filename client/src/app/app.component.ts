d873456c5c8ab8962cfc5e326c161f28ac5943e1
Ethan Dissmeyer <edissmeyer63342355@gmail.com>
2025-05-17 16:45:03 +1200

    navigation bar added, fixed positioning of dropdowns

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,13 +13,4 @@ export class AppComponent {
   title = 'client';
   constructor(private router: Router) {}
 
-  // Navigate to the email generator page
-  navigateToEmailGenerator(): void {
-    this.router.navigate(['/email-generator']);
-  }
-
-  // Navigate back to the previous page
-  goBack(): void {
-    this.router.navigate(['/']); // Adjust the route as needed
-  }
 }
