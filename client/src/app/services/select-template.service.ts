ed2ff5de9f5140273afa7ad04535b96140e0881c
Ethan Dissmeyer <edissmeyer63342355@gmail.com>
2025-05-20 17:16:43 +1200

    fix frontend for emailtemplate

--- client/src/app/services/select-template.service.ts
+++ client/src/app/services/select-template.service.ts
@@ -1,14 +1,19 @@
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SelectTemplateService {
-  private http = inject(HttpClient);
   private baseUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
 
   getTemplates(): Observable<any[]> {
     return this.http.get<any[]>(`${this.baseUrl}/templates`);
+  }
+
+  getTemplatesByTransactionType(type: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/templates/transaction-type/${encodeURIComponent(type)}`);
   }
 }
